e9201bfa2d395cf3bc6d569c644839f20055ffea
Paul Nordlund <paul.nordlund@abbvie.com>
2021-02-16 15:52:16 -0600

    remove irrelevant files

--- src/components/Shared/helpers.jsx
+++ src/components/Shared/helpers.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Popup } from 'semantic-ui-react';
 import _ from 'lodash';
-// import phosphosite_icon from '../../resources/phosphosite.ico';
-// import reactome_icon from '../../resources/reactome.jpg';
-// import go_icon from '../../resources/go.png';
-// import msig_icon from '../../resources/msig.ico';
 import * as d3 from 'd3-array';
-// import { omicNavigatorService } from '../../services/omicNavigator.service';
 
 export const Linkout = React.memo(
   function Linkout({ keyVar, itemValue, linkouts, favicons }) {
