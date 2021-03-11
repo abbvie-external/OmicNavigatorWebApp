e22a9de30a86d7fc91d7b8319082b4726dfc1487
Paul Nordlund <paul.nordlund@abbvie.com>
2021-03-11 11:14:55 -0600

    increment for release 1.1.2

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -55,7 +55,7 @@ class Tabs extends Component {
       allStudiesMetadata: [],
       differentialFeatureIdKey: '',
       filteredDifferentialFeatureIdKey: '',
-      appVersion: '1.1.1',
+      appVersion: '1.1.2',
       packageVersion: '',
     };
   }
