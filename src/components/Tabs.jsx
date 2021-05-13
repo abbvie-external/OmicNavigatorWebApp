553d21047d159655fd078ea0a7700d1c3c083d0a
Paul Nordlund <paul.nordlund@abbvie.com>
2021-05-13 11:48:39 -0500

    bump to 1.1.6, handle npm warnings

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -63,7 +63,7 @@ class Tabs extends Component {
       allStudiesMetadata: [],
       differentialFeatureIdKey: '',
       filteredDifferentialFeatureIdKey: '',
-      appVersion: '1.1.5',
+      appVersion: '1.1.6',
       packageVersion: '',
       infoOpenFirst: false,
       infoOpenSecond: false,
