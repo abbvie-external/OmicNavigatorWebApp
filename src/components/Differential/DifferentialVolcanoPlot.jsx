903ef72c9100151eaa99769ee5fd3e47fb5c060f
Joe Dalen <joe.dalen@abbvie.com>
2021-06-04 09:14:41 -0500

    Adds "NoSelect" class to tooltip

--- src/components/Differential/DifferentialVolcanoPlot.jsx
+++ src/components/Differential/DifferentialVolcanoPlot.jsx
@@ -833,6 +833,7 @@ class DifferentialVolcanoPlot extends React.PureComponent {
             )
             .attr('y', tooltipHeight <= 75 ? bin.y * 1 - 85 : bin.y * 1 + 10)
             .attr('id', 'tooltip')
+            .attr('class', 'NoSelect')
             .append('rect')
             .attr('width', '100%')
             .attr('height', '100%')
@@ -909,6 +910,7 @@ class DifferentialVolcanoPlot extends React.PureComponent {
                 : hoveredCircleData.position[1] * 1 + 10,
             )
             .attr('id', 'tooltip')
+            .attr('class', 'NoSelect')
             .append('rect')
             .attr('width', '100%')
             .attr('height', '100%')
