d7ee7b8e2d74d44b9d79f6c1105c9499ff42c2d6
Paul Nordlund <paul.nordlund@abbvie.com>
2021-09-20 13:09:40 -0500

    fix testIds argument

--- src/components/Differential/DifferentialSearchCriteria.jsx
+++ src/components/Differential/DifferentialSearchCriteria.jsx
@@ -215,9 +215,6 @@ class DifferentialSearchCriteria extends Component {
         });
         const differentialTestsMetadataVar =
           differentialModelWithTests?.tests || [];
-        const differentialTestIdsMapped = differentialTestsMetadataVar.map(
-          test => test.testID,
-        );
         const differentialTestsMapped = differentialTestsMetadataVar.map(
           test => {
             return {
@@ -227,13 +224,15 @@ class DifferentialSearchCriteria extends Component {
             };
           },
         );
-        const uDataPMapped = differentialTestsMetadataVar.map(t => t.testID);
+        const differentialTestIds = differentialTestsMetadataVar.map(
+          t => t.testID,
+        );
         this.setState({
           differentialTestsDisabled: false,
           differentialTests: differentialTestsMapped,
-          uDataP: uDataPMapped,
+          uDataP: differentialTestIds,
         });
-        this.props.onSetDifferentialTestIds(differentialTestIdsMapped);
+        this.props.onSetDifferentialTestIds(differentialTestIds);
         this.props.onSetTestsMetadata(differentialTestsMetadataVar);
         this.getReportLink(differentialStudy, differentialModel);
         if (differentialTest !== '') {
@@ -389,15 +388,15 @@ class DifferentialSearchCriteria extends Component {
         value: test.testID,
       };
     });
-    const uDataP = differentialTestsMetadataVar.map(t => t.testID);
+    const differentialTestIds = differentialTestsMetadataVar.map(t => t.testID);
     this.setState({
       differentialTestsDisabled: false,
       differentialTests: differentialTestsMapped,
-      uDataP: uDataP,
+      uDataP: differentialTestIds,
       differentialModelTooltip: differentialModelTooltip,
       differentialTestTooltip: '',
     });
-    this.props.onSetDifferentialTestIds(differentialTestsMapped);
+    this.props.onSetDifferentialTestIds(differentialTestIds);
     this.props.onSetTestsMetadata(differentialTestsMetadataVar);
     this.getReportLink(differentialStudy, value);
   };
