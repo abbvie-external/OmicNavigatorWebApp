48811d7476b6da57b598c50e1be1499e4f5f9ae8
Paul Nordlund <paul.nordlund@abbvie.com>
2022-01-25 15:13:39 -0600

    plotly style overrides

--- src/components/Differential/PlotsMultiFeature.jsx
+++ src/components/Differential/PlotsMultiFeature.jsx
@@ -15,6 +15,7 @@ import { roundToPrecision } from '../Shared/helpers';
 import ButtonActions from '../Shared/ButtonActions';
 import PlotlyMultiFeature from './PlotlyMultiFeature';
 import './PlotsDynamic.scss';
+import '../Shared/PlotlyOverrides.scss';
 
 class PlotsMultiFeature extends Component {
   state = {
