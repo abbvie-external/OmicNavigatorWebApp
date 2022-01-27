cb120fa5a89b4c603eb5a6fff4fd8f8f69e8d70b
Paul Nordlund <paul.nordlund@abbvie.com>
2022-01-27 16:10:37 -0600

    plot size updates

--- src/components/Differential/PlotsOverlay.jsx
+++ src/components/Differential/PlotsOverlay.jsx
@@ -71,19 +71,19 @@ class PlotsOverlay extends PureComponent {
     });
   };
 
-  getWidth() {
+  getWidth = () => {
     if (this.svgContainerRef.current !== null) {
-      return this.svgContainerRef.current.parentElement.offsetWidth;
+      return this.svgContainerRef.current.offsetParent.offsetWidth;
     }
     return 1200;
-  }
+  };
 
-  getHeight() {
+  getHeight = () => {
     if (this.svgContainerRef.current !== null) {
-      return this.svgContainerRef.current.parentElement.offsetWidth;
+      return this.svgContainerRef.current.offsetParent.offsetHeight;
     }
-    return 1200;
-  }
+    return 700;
+  };
 
   render() {
     const {
@@ -120,6 +120,7 @@ class PlotsOverlay extends PureComponent {
           const svgArray = [...this.props.plotOverlayData.svg];
           const svgPanes = svgArray.map(s => {
             const isPlotlyPlot = s.plotType.plotType.includes('plotly');
+            debugger;
             const svgContainerWidth = this.getWidth();
             const svgContainerHeight = this.getHeight();
             return {
@@ -127,11 +128,7 @@ class PlotsOverlay extends PureComponent {
               render: () => (
                 <Tab.Pane attached="true" as="div">
                   {isPlotlyPlot ? (
-                    <div
-                      id="PlotsOverlayContainer"
-                      className="svgSpan"
-                      ref={this.svgContainerRef}
-                    >
+                    <div id="PlotsOverlayContainer" className="svgSpan">
                       <PlotlyOverlay
                         plotlyData={s.svg}
                         height={svgContainerHeight}
@@ -261,7 +258,7 @@ class PlotsOverlay extends PureComponent {
                   largeScreen={16}
                   widescreen={16}
                 >
-                  <div className="">
+                  <div className="" ref={this.svgContainerRef}>
                     <Dropdown
                       search
                       selection
