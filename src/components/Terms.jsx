372d961017aed808cc8514070e41ab8635c42070
Paul Nordlund <paul.nordlund@abbvie.com>
2022-02-11 19:05:00 -0600

    POC

---
+++ src/components/Terms.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Modal, Button, Header } from 'semantic-ui-react';
+
+class Terms extends Component {
+  state = {
+    terms: null,
+    termsOpen: false,
+  };
+
+  componentDidMount() {
+    this.getTerms();
+  }
+
+  async getTerms() {
+    try {
+      let terms = await this.fetchTerms();
+      this.setState({
+        terms: terms,
+      });
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+
+  async fetchTerms() {
+    //   switch response to test file not that doesn't exist in public folder
+    let response = await fetch('/Terms.txt');
+    // let response = await fetch('/Terms.txt');
+    // read response stream as text
+    let textData = await response.text();
+    // we need to look for some specific string in the T&Cs because
+    // if the .txt file doesn't exist, it prints out the html as a string
+    if (textData.includes('jalapeno')) {
+      return textData;
+    } else return null;
+  }
+
+  toggleTerms = bool => {
+    this.setState({
+      termsOpen: bool,
+    });
+  };
+
+  getTermsButton = () => {
+    const { terms } = this.state;
+    const self = this;
+    return (
+      <>
+        <Button
+          className="CursorPointer"
+          id="TermsButton"
+          name="file"
+          onClick={() => self.toggleTerms(true)}
+        >
+          Terms
+        </Button>
+        <Modal
+          size="small"
+          closeOnDimmerClick={true}
+          closeOnEscape={true}
+          closeIcon
+          centered={false}
+          open={self.state.termsOpen}
+          onOpen={() => self.toggleTerms(true)}
+          onClose={() => self.toggleTerms(false)}
+        >
+          <Modal.Header>Terms &#38; Conditions</Modal.Header>
+          <Modal.Content image>
+            <Modal.Description>
+              <p>{terms}</p>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
+      </>
+    );
+  };
+
+  render() {
+    const { terms } = this.state;
+    const termsButton = this.getTermsButton();
+    return <>{terms && <span id="TermsButtonContainer">{termsButton}</span>}</>;
+  }
+}
+
+export default Terms;
