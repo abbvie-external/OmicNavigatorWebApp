84bb7762edd1a948cd6f588c406aa76d4fee29c8
Paul Nordlund <paul.nordlund@abbvie.com>
2023-08-14 09:37:45 -0500

    bump to v1.8.5

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -63,7 +63,7 @@ class Tabs extends Component {
       allStudiesMetadata: [],
       differentialFeatureIdKey: '',
       filteredDifferentialFeatureIdKey: '',
-      appVersion: '1.8.4',
+      appVersion: '1.8.5',
       packageVersion: '',
       infoOpenFirst: false,
       infoOpenSecond: false,
