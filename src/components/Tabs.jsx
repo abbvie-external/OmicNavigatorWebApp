e223a554142046588bc49b51e4aee59d5b53481d
Paul Nordlund <paul.nordlund@abbvie.com>
2024-01-22 18:12:49 -0600

    bump to v1.8.9

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -64,7 +64,7 @@ class Tabs extends Component {
       differentialFeatureIdKey: '',
       filteredDifferentialFeatureIdKey: '',
       // when updating the app version, change one line in 3 files: package.json, manifest.json and Tabs.jsx
-      appVersion: '1.8.8',
+      appVersion: '1.8.9',
       packageVersion: '',
       infoOpenFirst: false,
       infoOpenSecond: false,
