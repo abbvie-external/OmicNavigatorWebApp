766e9811a01b3e23eadf82c88f3a7166300878eb
Paul Nordlund <paulnordlund@gmail.com>
2025-08-27 11:33:41 -0500

    bump to v2.0.3

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -64,7 +64,7 @@ class Tabs extends Component {
       differentialFeatureIdKey: '',
       filteredDifferentialFeatureIdKey: '',
       // when updating the app version, change one line in 3 files: package.json, manifest.json and Tabs.jsx
-      appVersion: '2.0.2',
+      appVersion: '2.0.3',
       packageVersion: '',
       infoOpen: false,
       screenWidth: window.innerWidth,
